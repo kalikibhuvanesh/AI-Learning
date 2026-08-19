da69c4f55e17dde0d51134d51e0c2400880a49ea
Kaliki Bhuvanesh <kalikibhuvanesh4@gmail.com>
2026-08-19 19:47:58 +0530

    Add MNIST 0 vs 1 neural network

---
+++ Neural_Networks/MNIST_0_vs_1/imageclassification.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from PIL import Image
+import matplotlib.pyplot as plt
+from tensorflow.keras.losses import BinaryCrossentropy
+from tensorflow.keras.layers import Dense,Flatten
+from tensorflow.keras import Sequential
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+train_filter = (y_train == 0) | (y_train == 1)
+x_train = x_train[train_filter]
+y_train = y_train[train_filter]
+test_filter = (y_test == 0) | (y_test == 1)
+x_test = x_test[test_filter]
+y_test = y_test[test_filter]
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+model = Sequential([Flatten(input_shape=(28,28)),Dense(units=128,activation='relu'),Dense(units=1,activation='sigmoid')])
+model.compile(loss = 'binary_crossentropy',optimizer ='adam',metrics = ['accuracy'])
+model.fit(x_train,y_train,epochs=5,batch_size=32)
+test_loss, test_accuracy = model.evaluate(x_test, y_test)
+print("Test Loss:", test_loss)
+print("Test Accuracy:", test_accuracy)
---
+++ README.md
@@ -0,0 +1,58 @@
+\# AI Learning
+
+
+
+This repository contains my journey of learning Artificial Intelligence.
+
+
+
+\## Topics
+
+
+
+\- Machine Learning
+
+\- Neural Networks
+
+\- Deep Learning
+
+\- Computer Vision
+
+\- Natural Language Processing
+
+\- Generative AI
+
+
+
+\## Current Progress
+
+
+
+\### Neural Networks
+
+
+
+\#### MNIST 0 vs 1 Classification
+
+
+
+\- Loaded MNIST dataset using TensorFlow
+
+\- Filtered images to 0 and 1
+
+\- Normalized pixel values
+
+\- Built a neural network
+
+\- Trained the model
+
+\- Evaluated the model on unseen test data
+
+
+
+Training Accuracy: \~99.97%
+
+
+
+Test Accuracy: \~99.95%
+
